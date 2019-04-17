d092741f9b9cfb29229b55007ab871171391f200
Mihai Hodorogea <mihai.hodorogea@trsb.net>
2019-04-17 15:55:08 +0200

    wip

---
+++ AccountOwnerServer/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+
+namespace AspNetCoreWebApiRepositoryPattern
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["mysqlconnection:connectionString"];
+            services.AddDbContext<RepositoryContext>(o => o.UseSqlServer(connectionString));
+        }
+
+
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+        }
+    }
+
+}
---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class OwnerRepository : RepositoryBase<Owner>, IOwnerRepository
+    {
+        public OwnerRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
